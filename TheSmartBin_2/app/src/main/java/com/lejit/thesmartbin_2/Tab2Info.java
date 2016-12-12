a8219f7087273fd26aedd05b90099b7c90e42a37
sitaraja <sita.a.rajagopal@gmail.com>
2016-12-12 21:00:03 +0800

    firebase update

--- TheSmartBin_2/app/src/main/java/com/lejit/thesmartbin_2/Tab2Info.java
+++ TheSmartBin_2/app/src/main/java/com/lejit/thesmartbin_2/Tab2Info.java
@@ -12,24 +12,45 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemSelectedListener;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 
 /**
  * Created by MY LENOVO on 12/12/2016.
  */
 
 public class Tab2Info extends Fragment {
+    Button submit;
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.ground_tab2, container, false);
         String [] values =
                 {"Select Bin Status","Empty","25%","50%","75%","Full",};
-        Spinner spinner = (Spinner) rootView.findViewById(R.id.spinner);
+        final Spinner spinner = (Spinner) rootView.findViewById(R.id.spinner);
+
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, values);
         adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
         spinner.setAdapter(adapter);
+        submit=(Button) rootView.findViewById(R.id.submitButton);
+        Firebase.setAndroidContext(this.getActivity());
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String text = spinner.getSelectedItem().toString();
+                if(text=="25%"||text=="50%"||text=="75%"){
+                    text=Character.toString(text.charAt(0))+Character.toString(text.charAt(1))+"pFilled";
+                }
+                Firebase myFirebaseRef=new Firebase("https://smartbin-16031.firebaseio.com");
+                myFirebaseRef.child("message").setValue(text);
+
+            }
+        });
+
         return rootView;
 
     }
